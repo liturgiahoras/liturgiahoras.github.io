f30764c4b55bba186212b1cbd35f7d6bfa7e39d5
Ramón Fandos <fandosrj@gmail.com>
2026-09-10 17:40:10 +0200

    Estadísticas anónimas con GoatCounter (sin cookies); textos de privacidad (SW v9)

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Precarga la app y la librería de rezo (una sola vez).
    ============================================================ */
 
-const CACHE = 'liturgia-horas-v8';
+const CACHE = 'liturgia-horas-v9';
 const CORE = [
   './',
   './index.html',
